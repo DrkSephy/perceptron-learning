34bae02f0589744aaacea537b83257bcab586ed6
DrkSephy <dleonar00@citymail.cuny.edu>
2016-04-25 16:56:44 -0400

    Converting input and output vectors to bipolar

--- perceptron.py
+++ perceptron.py
@@ -2,17 +2,17 @@ from random import choice
 from numpy import array, dot, random
 
 # Create unit step function
-unit_step = lambda x: 0 if x < 0 else 1
+unit_step = lambda x: -1 if x < 0 else 1
 
 # Map input to output
 # First two values in the array are the inputs
 # Third value is the bias
 # Last value is the expected output
 training_data = [
-  (array([0, 0, 1]), 0),
-  (array([0, 1, 1]), 1),
-  (array([1, 0, 1]), 1),
-  (array([1, 1, 1]), 1),
+  (array([+1, +1, +1]), +1),
+  (array([+1, -1, +1]), -1),
+  (array([-1, +1, +1]), -1),
+  (array([-1, -1, +1]), -1),
 ]
 
 # Initialize the weights randomly
@@ -41,7 +41,7 @@ while True:
     # Compute the error
     error = expected - unit_step(result)
 
-    # If there was en error, update the weights
+    # If there was an error, update the weights
     if error != 0:
       # Increment error count by 1
       error_count += 1
